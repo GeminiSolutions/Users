6ae36f374b5bb1925d61b0a6b87ab9111f7be58b
Vlad Hociotă <vlad.hociota@geminisols.com>
2017-05-12 15:23:23 +0300

    [fix]

--- Sources/UsersList.swift
+++ Sources/UsersList.swift
@@ -14,7 +14,7 @@ public class UsersList: DataStoreContentJSONArray<User.JSONObjectType> {
     }
 
     public init(usersData: [(user: User.JSONObjectType, id: User.UserIdType, lastUpdate: Date)]) {
-        let users = usersData.map({ return ["user":$0.user, "id":User.stringFromUserId($0.id), "lastUpdate":UsersList.dateString(from: $0.lastUpdate)] })
+        let users = usersData.map({ return ["content":$0.user, "id":User.stringFromUserId($0.id), "lastUpdate":UsersList.dateString(from: $0.lastUpdate)] })
         super.init(json: users)
     }
 
@@ -25,7 +25,7 @@ public class UsersList: DataStoreContentJSONArray<User.JSONObjectType> {
         }
         return users
     }
-    
+
     public func user<UserType: User>(for id: User.UserIdType) -> UserType? {
         for data in content {
             if let idStr = data["id"] as? String, let userId = User.userIdFromString(idStr), userId == id {
@@ -37,11 +37,11 @@ public class UsersList: DataStoreContentJSONArray<User.JSONObjectType> {
 
     public func append(user: User) {
         guard let id = user.id, let lastUpdate = user.lastUpdate else { return }
-        append(["user":user.content, "id":User.stringFromUserId(id), "lastUpdate":UsersList.dateString(from: lastUpdate)])
+        append(["content":user.content, "id":User.stringFromUserId(id), "lastUpdate":UsersList.dateString(from: lastUpdate)])
     }
 
     private func user<UserType: User>(from data: [String:Any]) -> UserType? {
-        guard let content = data["user"] as? User.JSONObjectType, let id = data["id"] as? String, let lastUpdate = data["lastUpdate"] as? String else { return nil }
+        guard let content = data["content"] as? User.JSONObjectType, let id = data["id"] as? String, let lastUpdate = data["lastUpdate"] as? String else { return nil }
         guard let userId = User.userIdFromString(id), let userLastUpdate = UsersList.date(from: lastUpdate) else { return nil }
         guard let user = UserType(content: content) else { return nil }
         user.id = userId
