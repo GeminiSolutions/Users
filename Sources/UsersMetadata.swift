dfb0064564a6da83be76f33899dbd1be1eec13b4
Vlad Hociotă <vlad.hociota@geminisols.com>
2017-05-25 12:35:35 +0300

    [fix]

--- Sources/UsersMetadata.swift
+++ Sources/UsersMetadata.swift
@@ -8,7 +8,7 @@
 import Foundation
 import DataStore
 
-public class UsersMetadata: DataStoreItemsMetadataJSON {
+open class UsersMetadata: DataStoreItemsMetadataJSON {
     public var fields: [[String:Any]]? {
         get {
             return content["fields"] as? [[String:Any]]
